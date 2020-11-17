20299e338faea51357e2dce61de6c06684287595
ariel_grubbs <arielvictor1234@gmail.com>
2020-11-16 21:02:42 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let object1 = array.find(object => object.result === "W")
+  console.log(object1)
+  if (object1) {
+    return object1.year
+  } else {
+    return object1
+  }
+}
